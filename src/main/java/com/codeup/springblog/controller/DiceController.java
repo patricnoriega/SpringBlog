1243ffc33fbad066a7c994b4b88c2e2bdd9d93dc
patric noriega <patricnoriega@gmail.com>
2022-01-03 15:27:42 -0600

    updated commit

---
+++ src/main/java/com/codeup/springblog/controller/DiceController.java
@@ -0,0 +1,16 @@
+package com.codeup.springblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DiceController {
+//    @RequestMapping(path = "/roll-dice")
+//    @ResponseBody
+//    public int random() {
+
+//    }
+}
