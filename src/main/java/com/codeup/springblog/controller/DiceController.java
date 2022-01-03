0343e5193e51bf5d1dec7ec8119c82ed55bc87a5
patric noriega <patricnoriega@gmail.com>
2022-01-03 16:42:05 -0600

    throwing dice

--- src/main/java/com/codeup/springblog/controller/DiceController.java
+++ src/main/java/com/codeup/springblog/controller/DiceController.java
@@ -1,16 +1,25 @@
 package com.codeup.springblog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @Controller
 public class DiceController {
-//    @RequestMapping(path = "/roll-dice")
-//    @ResponseBody
-//    public int random() {
+    @GetMapping("/roll-dice/{n}")
+    public String rollDiceUser(@PathVariable int n, Model model) {
+        int random = ThreadLocalRandom.current().nextInt(1,6+1);
+        model.addAttribute("randomNumber", random);
+        model.addAttribute("userGuess", n);
+        model.addAttribute("isUserGuessCorrect", n == random);
+        return "dice";
+    }
 
-//    }
+    @GetMapping("/roll-dice")
+    public String rollDice() {
+        return "dice";
+    }
 }
