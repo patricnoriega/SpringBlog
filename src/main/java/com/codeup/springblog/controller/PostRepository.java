32f8de93aeef18e3953841b17ab6188f350ae4e4
patric noriega <patricnoriega@gmail.com>
2022-01-05 11:01:03 -0600

    uncommited files from yesterday

--- src/main/java/com/codeup/springblog/controller/PostRepository.java
+++ src/main/java/com/codeup/springblog/controller/PostRepository.java
@@ -3,8 +3,5 @@ package com.codeup.springblog.controller;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-//    Post findById(long id);
-//    Post findPostsById(long id);
-//    Post findPostById(long id);
-//    Post getPostById(long id);
+
 }
